f0cd2f66e58a86dc3d08e3b450e4141ecde3bbf4
vkasyanov12 <kasyanov@my.canisius.edu>
2018-11-11 21:54:34 -0500

    Made the portfolio class use multiprocessing

--- Portfolio.py
+++ Portfolio.py
@@ -5,9 +5,11 @@ from dateutil.relativedelta import *
 import pandas as pd
 from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
 import math
+from multiprocessing import Pool, freeze_support
+import os
+import numpy as np
 
 class Portfolio:
-
     def __init__(self,strategy):
         self.strategies = ["momentum win/lose"]
         self.markets = get_nasdaq_symbols().index
@@ -15,13 +17,55 @@ class Portfolio:
         if self.portfolio_strategy == False:
             raise ValueError("No such strategy exists")
         self.portfolio = None
+        self.start = None
+        self.end = None
 
     def check_strategy(self,strategy):
         if strategy in self.strategies:
             return strategy
         else:
             return False
-
+    
+    def momen_calc(self,stock):
+        try:
+            current_asset = mk.Asset(stock,start=self.start.strftime('%Y%m%d'),end=self.end.strftime('%Y%m%d'))
+            start_value = current_asset.candles.close.loc[self.start.strftime('%Y-%m-%d')]
+            end_value = current_asset.candles.close.loc[self.end.strftime('%Y-%m-%d')]
+            return [stock,((end_value-start_value)/start_value) * 100,None]
+        except:
+            return [stock,float('nan'),None]
+        
+    
+    def momen_win_loss(self,time_period=6):
+        self.start = (datetime.now() - relativedelta(years=5)) + relativedelta(days=+3)
+        #self.start = (datetime.now() - relativedelta(years=5)) + relativedelta(days=+1) #date starts 4 years and 364 days 
+        #self.end = self.start + relativedelta(months=+time_period)
+        self.end = self.start + relativedelta(months=+time_period) + relativedelta(days=+3)
+        
+        
+        #stocks = ["aapl","NVDA","amzn","amd","goog","F","INTC"]
+        stocks = self.markets[:200]
+        #print(len(self.markets))
+        
+        start_time = datetime.now()
+        
+        pool = Pool(os.cpu_count())
+        
+        results = pool.map(self.momen_calc,stocks)
+        pool.close()
+        pool.join()
+        
+        #cut_off = math.floor(len(portfolio)*.1)
+        self.portfolio = pd.DataFrame(results,columns = ["stock","change","position"]).dropna(subset=["change"])
+        self.portfolio = self.portfolio.sort_values(by=['change'],ascending=False)
+        end_time = datetime.now()
+        
+        
+        print(end_time-start_time)
+        print(self.portfolio)
+        
+        
+    '''
     def momen_win_loss(self,time_period=6):
         start = (datetime.now() - relativedelta(years=5)) + relativedelta(days=+1) #date starts 4 years and 364 days 
         end = start + relativedelta(months=+time_period)
@@ -46,10 +90,12 @@ class Portfolio:
         portfolio[-cut_off:].position = "Short" #assigns short position in the bottom 10%
         self.portfolio = portfolio.copy()
         return self.portfolio
-        
+    ''' 
 def main():
     a = Portfolio("momentum win/lose")
     print("done")
     print(a.momen_win_loss())
 
-main()
+if __name__ == '__main__':
+	freeze_support()
+	main()
